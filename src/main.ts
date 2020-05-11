0009c594155f80e6acf8ca95e8bcfb0f0a228b90
Nursultan <abdazovnursultan@gmail.com>
2020-05-11 12:25:36 +0600

    feat(env): port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { Logger } from '@nestjs/common';
 
+const port = process.env.PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`Server started running onhttp://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
