1287a78b0c348714e090a73a27f5066b9938655a
Nursultan <abdazovnursultan@gmail.com>
2020-07-11 19:22:12 +0600

    fix

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('swagger', app, document);
   await app.listen(port);
-  Logger.log(`Server started running onhttp://localhost:${port}`, 'Bootstrap');
+  Logger.log(`Server started running on http://localhost:${port}`, 'Bootstrap');
 }
 
 bootstrap();
